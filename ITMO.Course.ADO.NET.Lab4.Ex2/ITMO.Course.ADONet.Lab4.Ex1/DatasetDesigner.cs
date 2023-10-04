65d18d06e8733ac09c6f8555a3873df7195e646d
Антон Сбитнев <balamut86@list.ru>
2023-10-05 00:16:41 +0300

    Добавьте файлы проекта.

---
+++ ITMO.Course.ADO.NET.Lab4.Ex2/ITMO.Course.ADONet.Lab4.Ex1/DatasetDesigner.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ITMO.Course.ADONet.Lab4.Ex1
+{
+    public partial class DatasetDesigner : Form
+    {
+        public DatasetDesigner()
+        {
+            InitializeComponent();
+        }
+
+        private void CustomersListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void GetCustomersButton_Click(object sender, EventArgs e)
+        {
+            NorthwindDataSet NorthwindDataset1 = new NorthwindDataSet();
+            NORTHWNDDataSetTableAdapters.CustomersTableAdapter CustomersTableAdapter1 = new NORTHWNDDataSetTableAdapters.CustomersTableAdapter();
+            CustomersTableAdapter1.Fill(NorthwindDataset1.Customers);
+            foreach (NorthwindDataSet.CustomersRow NWCustomer in NorthwindDataset1.Customers.Rows)
+            {
+                CustomersListBox.Items.Add(NWCustomer.CompanyName);            
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
